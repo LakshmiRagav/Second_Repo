294c095adc3112d9c69af5e80a31bab36740eff6
LakshmiRagav <ragavkrishna12@gmail.com>
2019-12-14 07:00:51 -0500

    Added First change in EClipse code

--- SecondJava_project/src/com/walmart/com/Kitchensupplies.java
+++ SecondJava_project/src/com/walmart/com/Kitchensupplies.java
@@ -3,7 +3,7 @@ package com.walmart.com;
 public class Kitchensupplies {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Walmary Shopping");
 
 	}
 
