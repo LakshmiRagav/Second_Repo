506172cd4985a20728972ee9265b998939d4064f
LakshmiRagav <ragavkrishna12@gmail.com>
2019-12-14 07:34:38 -0500

    Fourth change

--- SecondJava_project/src/com/walmart/com/Kitchensupplies.java
+++ SecondJava_project/src/com/walmart/com/Kitchensupplies.java
@@ -2,6 +2,8 @@ package com.walmart.com;
 
 public class Kitchensupplies {
 
+	int a=10,b=20,c;
+	
 	public static void main(String[] args) {
 		System.out.println("Walmary Shopping");
 		System.out.println("second change");
