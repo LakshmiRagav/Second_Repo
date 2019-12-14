8ab636b5b12b630833b8d6324039b2f25ea03cd9
LakshmiRagav <ragavkrishna12@gmail.com>
2019-12-14 07:43:57 -0500

    Atlanta changes

--- SecondJava_project/src/com/walmart/com/Kitchensupplies.java
+++ SecondJava_project/src/com/walmart/com/Kitchensupplies.java
@@ -2,6 +2,8 @@ package com.walmart.com;
 
 public class Kitchensupplies {
 
+	int x=100,y=200,z;
+	
 	public static void main(String[] args) {
 		System.out.println("Walmary Shopping");
 		System.out.println("second change");
