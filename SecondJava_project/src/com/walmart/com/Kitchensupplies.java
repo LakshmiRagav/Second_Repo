e9fe16ee87430d2269baa704f372781691354c55
LakshmiRagav <ragavkrishna12@gmail.com>
2019-12-14 07:08:37 -0500

    Third change in Kitchen class

--- SecondJava_project/src/com/walmart/com/Kitchensupplies.java
+++ SecondJava_project/src/com/walmart/com/Kitchensupplies.java
@@ -5,6 +5,7 @@ public class Kitchensupplies {
 	public static void main(String[] args) {
 		System.out.println("Walmary Shopping");
 		System.out.println("second change");
+		System.out.println("Third change");
 
 	}
 
